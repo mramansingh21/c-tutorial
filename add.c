950ea292871ad220baaacebd0593f9435bb43492
mramansingh21 <155548204+mramansingh21@users.noreply.github.com>
2024-01-03 18:48:41 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    printf ("\nEnter a number:");
+    scanf ("%d", &a);
+ 
+    printf ("\nEnter another number:");
+    scanf ("%d", &b);
+    
+    printf ("\nthe sum is: %d\n",a+b); 
+
+
+
+    
+    return 0;
+}
