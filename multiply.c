4a51d21ab76fc97b1285e85ae7725880f61dc985
mramansingh21 <155548204+mramansingh21@users.noreply.github.com>
2024-01-03 18:46:25 +0530

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+  int a,b,d;
+  
+  printf ("\nEnter a number: ");
+  scanf ("%d", &a);
+  
+  printf ("\nEnter another number: ");
+  scanf ("%d", &b);
+  
+  d=a*b;
+  printf ("\nThe Multiplication is= %d\n", d ); 
+   
+  printf ("\aFinally code in running!");
+ 
+  return 0;
+
+}
