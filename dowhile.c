302ce349a2d56a7cf1951be636eff8ed3010f67c
mramansingh21 <155548204+mramansingh21@users.noreply.github.com>
2024-01-11 21:48:09 +0530

    Aman singh

---
+++ dowhile.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+   int a,b=0 ;
+
+    printf("\nEnter a number: ");
+    scanf("%d", &a);
+
+    do
+    { 
+        printf("%d\n", b+1);
+         b=b+1;
+     
+      } while (b<a);
+    
+      return 0;
+
+}
