1dc20ee7cf728ec4120a8a0fdf465e8ee73d6328
mramansingh21 <155548204+mramansingh21@users.noreply.github.com>
2024-01-18 22:38:25 +0530

    Aman singh

---
+++ typecasting.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{   
+    int a=6;
+    //float b=(float)5/3;
+   float b=7;
+        printf("The value of a is %d\n", (int)b );
+     
+    // typecasting syntax
+     // (type) value;
+
+    return 0;
+}
